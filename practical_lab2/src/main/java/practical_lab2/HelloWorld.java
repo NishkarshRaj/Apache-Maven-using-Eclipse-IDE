3d4d409f83b8d500d959c151e1e038bbe536d0f8
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-06-21 11:09:26 +0530

    Practical 2 completed

---
+++ practical_lab2/src/main/java/practical_lab2/HelloWorld.java
@@ -0,0 +1,8 @@
+package practical_lab2;
+
+public class HelloWorld {
+	public static void main()
+	{
+		System.out.println("Hello World!");
+	}
+}
---
+++ practical_lab2/src/test/java/practical_lab2/HelloTest.java
@@ -0,0 +1,14 @@
+package practical_lab2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
