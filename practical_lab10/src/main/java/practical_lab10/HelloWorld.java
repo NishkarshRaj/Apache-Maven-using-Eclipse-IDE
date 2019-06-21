0cc2cd25e296dc09f28c95cc14a591c7b1fb7e49
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-06-21 12:58:44 +0530

    Lab 10 Done! Target Created!

---
+++ practical_lab10/src/main/java/practical_lab10/HelloWorld.java
@@ -0,0 +1,12 @@
+package practical_lab10;
+
+public class HelloWorld {
+	public void function1()
+	{
+		System.out.println("Hello World!!");
+	}
+	public static void main(String args[])
+	{
+		System.out.println("Hello World!!!!!");
+	}
+}
---
+++ practical_lab10/src/test/java/practical_lab10/Test.java
@@ -0,0 +1,12 @@
+package practical_lab10;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
